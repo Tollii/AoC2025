023f55ff21ce478ef856c25d3f27f7f06c364f5d
Andreas Tolnes <andreas.tolnes@soprasteria.com>
2025-12-01 19:43:54 +0100

    Day one

---
+++ AoC2025/DayOne/DayOne.fsx
@@ -0,0 +1,46 @@
+open System.IO
+
+Directory.SetCurrentDirectory __SOURCE_DIRECTORY__
+
+type Direction =
+    | Left
+    | Right
+    
+let parse (line: string) =
+                               match line[0] with
+                               | 'L' -> Left, (int line[1..])
+                               | 'R' -> Right, (int line[1..])
+                               | _ -> failwith "Invalid input"
+                               
+let wrap value = ((value % 100) + 100 ) % 100
+
+let moveLeft amount dial =
+    let newPosition = wrap (dial - amount)
+    let zeroPasses = match dial with
+                     | 0 -> amount / 100
+                     | dial when dial > amount -> 0
+                     | _ -> 1 + (amount - dial) / 100
+    newPosition, zeroPasses
+    
+let moveRight amount dial =
+    let newPosition = wrap (dial + amount)
+    let zeroPasses = ((dial + amount) / 100)
+    newPosition, zeroPasses
+                               
+let result = File.ReadLines "input.txt"
+                    |> Seq.toList
+                    |> List.map parse
+                    |> List.map (fun (direction, amount) -> match direction with
+                                                            | Left -> moveLeft amount
+                                                            | Right -> moveRight amount)
+                    |> List.scan (fun (newPosition, _) moveFunc -> moveFunc newPosition) (50, 0)
+
+let partOneAnswer = result
+                               |> List.filter (fun (position, _) -> position = 0)
+                               |> List.length
+                 
+let partTwoAnswer = result
+                    |> List.sumBy snd
+                 
+printfn "Password 1 is: %d" partOneAnswer
+printfn "Password is: %d" partTwoAnswer
